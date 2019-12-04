e15a4203b6fe193caeaee770ab2369ea89f5fe25
Quese22 <MarqueseMhayes@gmail.com>
2019-12-04 10:38:05 -0500

    saving

--- src/app/components/Outfit/outfit-create/outfit-create.component.ts
+++ src/app/components/Outfit/outfit-create/outfit-create.component.ts
@@ -4,6 +4,10 @@ import { OutfitsService } from 'src/app/services/outfits.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LocationModel } from 'src/app/models/location';
 import { ActionCreateComponent } from '../../action/action-create/action-create.component';
+import { ActionsService } from 'src/app/services/actions.service';
+import { Outfit } from 'src/app/models/Outfit';
+import { Action } from 'rxjs/internal/scheduler/Action';
+
 
 
 @Component({
@@ -14,51 +18,61 @@ import { ActionCreateComponent } from '../../action/action-create/action-create.
 export class OutfitCreateComponent implements OnInit {
 
   outfitForm: FormGroup;
-  location:LocationModel
-  Action:ActionCreateComponent
-  constructor(private outfitService: OutfitsService, private form: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute) {
-    
-   }
+  location:LocationModel;
+  Action:ActionCreateComponent;
+  outfitToBeSubmitted: Outfit;
+  actionToBeSubmitted: Action;
+  constructor(private outfitService: OutfitsService, private form: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute)
+  {}
 
+
+    
+  
   ngOnInit() {
   }
-
+  
   createForm(){
     this.outfitForm = this.form.group({
       OutfitName: new FormControl,
       Top: new FormControl,
       Bottom: new FormControl,
       OutfitID: new FormControl,
-      ActionActivity: new FormControl,
-      ActionATempRange: new FormControl
-
+      
     });
   }
-
-    onSubmit() {
-      var outfitCreate = {
-OutfitName: this.outfitForm.value.OutfitName,
-Top: this.outfitForm.value.Top,
-Bottom: this.outfitForm.value.Bottom,
-OutfitID: this.outfitForm.value.OutfitID
-      }
-      var actionCreate = {
-ActionActivity: this.outfitForm.value.Activity,
-ActionAtempRange: this.outfitForm.value.AtempRange
-      }
-      this.activatedRoute.paramMap.subscribe(routeData=> {
-        this.outfitService.getOutfits(routeData.get('locationId'))
-        .subscribe((locationn:LocationModel)=>{
-          this.location=locationn;
-      this.outfitService.createOutfits(locationn.LocationID,this.outfitForm.value)
-      .subscribe(data=>{
-        this.router.navigate([''])
-      });
-    });
-  });
+  
+//   onSubmit() {
+//     //  var outfitCreate = {
+//       //OutfitName: this.outfitForm.value.OutfitName,
+//       //Top: this.outfitForm.value.Top,
+//       //Bottom: this.outfitForm.value.Bottom,
+//       //OutfitID: this.outfitForm.value.OutfitID
+//       //   }
+//       //var actionCreate = {
+//         //ActionActivity: this.outfitForm.value.Activity,
+// //ActionAtempRange: this.outfitForm.value.AtempRange
+// //   }
+// this.activatedRoute.paramMap.subscribe(routeData=> {
+//   this.outfitService.getOutfits(routeData.get('locationId'))
+//   .subscribe((locationn:LocationModel)=>{
+//           this.location=locationn;
+//           this.outfitService.createOutfits(locationn.LocationID,this.outfitForm.value)
+//           .subscribe(data=>{
+//             this.router.navigate([''])
+            
+//             this.activatedRoute.paramMap.subscribe(routeData=> {
+//               this.actionService.getAction(routeData.get('locationId')).subscribe((locationn:LocationModel)=>{
+//                 this.location=locationn;
+//                 this.actionService.createAction(locationn.LocationID,this.actionForm.value).subscribe(data=>{
+//                   this.router.navigate([''])
+//                 });
+//     });
+//   });
+  
+// }
 
 }
 
-}
+
 
 
